52a3276014c9bffb8dcf5b9e9f8eb62a2ad4c1e1
Marcus PC900 <mdhi4831@uni.sydney.edu.au>
2024-08-25 02:51:38 +1000

    allow cors

--- app.py
+++ app.py
@@ -7,7 +7,4 @@ everything else is the the /backend dir.
 from backend import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app, 
-                 port="38433", 
-                 host="0.0.0.0",
-                 debug=True)
+    socketio.run(app, port="38433", host="0.0.0.0", debug=True)
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask 
 from flask_socketio import SocketIO 
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 socketio = SocketIO(app)
 
