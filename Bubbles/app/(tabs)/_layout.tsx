c0c16e3589cf09ac4945e67421f6ee906c3c0285
shionjjl <143237303+shionjjl@users.noreply.github.com>
2024-08-24 23:53:09 +1000

    added chat tab

--- Bubbles/app/(tabs)/_layout.tsx
+++ Bubbles/app/(tabs)/_layout.tsx
@@ -24,6 +24,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
